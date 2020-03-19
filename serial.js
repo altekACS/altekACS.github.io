56c977f06897d9c18a82c44de9296a56b95a3d79
Jerome Chuang <daiman411@users.noreply.github.com>
2020-03-19 19:50:54 +0800

    Update PID & VID for my webcam

--- serial.js
+++ serial.js
@@ -23,6 +23,7 @@ var serial = {};
       { 'vendorId': 0x2341, 'productId': 0x8055 }, // Arduino MKR NB 1500
       { 'vendorId': 0x2341, 'productId': 0x8056 }, // Arduino MKR Vidor 4000
       { 'vendorId': 0x2341, 'productId': 0x8057 }, // Arduino NANO 33 IoT
+      { 'vendorId': 0x1908, 'productId': 0x2310 }, // USB 2.0 PC CAMERA
       { 'vendorId': 0x239A }, // Adafruit Boards!
     ];
     return navigator.usb.requestDevice({ 'filters': filters }).then(
