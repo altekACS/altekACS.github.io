3f6399faa3888ea1d4125ecf90da8295fd9ddb2d
JeromeChuang <JeromeChuang@altek.com.tw>
2025-02-11 10:23:05 +0800

    Refactor stock price service to return JSON objects instead of JSON strings for error handling and response data

--- DailyShare/stock_price_service.py
+++ DailyShare/stock_price_service.py
@@ -8,7 +8,7 @@ def get_stock_price(ticker = None):
     # ticker = request.args.get('ticker')
     if not ticker:
         res['status_code'] = 400
-        res['json'] = json.dumps({"error": "Missing ticker parameter"})
+        res['json'] = {"error": "Missing ticker parameter"}
         return res
 
     stock = yf.Ticker(ticker)
@@ -16,15 +16,17 @@ def get_stock_price(ticker = None):
 
     if stock_info.empty:
         res['status_code'] = 404
-        res['json'] = json.dumps({"error": "Invalid ticker or no data available"})
+        res['json'] = {"error": "Invalid ticker or no data available"}
         return res
 
     latest_price = stock_info["Close"].iloc[-1]
     res['status_code'] = 200
-    res['json'] = json.dumps({"ticker": ticker, "price": latest_price})
+    res['json'] = {"ticker": ticker, "price": latest_price}
     return res
 
 # if __name__ == '__main__':
 #     response = get_stock_price('2330.TW')
-#     print(response.json)
+#     stock_data = response['json']
+#     company_price = stock_data["price"]
+#     print(company_price)
         
